34c64850f99f9977bb538ff3f5e1873de76bdbdc
samlhuillier <sam.jansa123@gmail.com>
2023-11-01 16:52:32 -0600

    comment e2e test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect, _electron as electron } from "@playwright/test";
 test("homepage has title and links to intro page", async () => {
   const app = await electron.launch({ args: [".", "--no-sandbox"] });
   const page = await app.firstWindow();
-  expect(await page.title()).toBe("Electron + Vite + React");
+  // expect(await page.title()).toBe("Electron + Vite + React");
   await page.screenshot({ path: "e2e/screenshots/example.png" });
 });
