10bc043aba7ec7fe71f30f1ba678f22888c3b520
samlhuillier <sam.jansa123@gmail.com>
2023-11-01 16:11:19 -0600

    tailwind baby

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/global.css";
+
 // import './samples/node-api'
 // import './index.css'
 
