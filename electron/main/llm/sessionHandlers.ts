2443ef1b9a7fde5ae7673ae2f839c854c639ad7f
samlhuillier <sam.jansa123@gmail.com>
2023-11-22 15:53:48 -0600

    session handler code

---
+++ electron/main/llm/sessionHandlers.ts
@@ -0,0 +1,31 @@
+import { ipcMain, IpcMainInvokeEvent } from "electron";
+import { ModelLoader, SessionService } from "./LlamaCpp"; // Assuming SessionService is in the same directory
+
+const modelLoader = new ModelLoader(); // Singleton
+const sessions: { [sessionId: string]: SessionService } = {};
+
+export const registerSessionHandlers = () => {
+  ipcMain.handle(
+    "createSession",
+    async (event: IpcMainInvokeEvent, sessionId: string) => {
+      if (sessions[sessionId]) {
+        throw new Error(`Session ${sessionId} already exists.`);
+      }
+      const webContents = event.sender;
+      const sessionService = new SessionService(modelLoader, webContents);
+      sessions[sessionId] = sessionService;
+      return sessionId;
+    }
+  );
+
+  ipcMain.handle(
+    "initializeStreamingResponse",
+    async (event: IpcMainInvokeEvent, sessionId: string, prompt: string) => {
+      const sessionService = sessions[sessionId];
+      if (!sessionService) {
+        throw new Error(`Session ${sessionId} does not exist.`);
+      }
+      return sessionService.streamingPrompt(prompt);
+    }
+  );
+};
