05f3d0812b61dac035e8d7b7a2f4c16f0dca69df
samlhuillier <sam.jansa123@gmail.com>
2023-11-22 16:35:57 -0600

    style improvements to chatbot

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -22,16 +22,6 @@ const ChatWithLLM: React.FC = () => {
     };
   }, []);
 
-  // useEffect(() => {
-  //   if (currentBotMessage) {
-  //     // Only update the messages array when a new user message is sent
-  //     setMessages((prevMessages) => [
-  //       ...prevMessages,
-  //       { sender: "bot", text: currentBotMessage },
-  //     ]);
-  //   }
-  // }, [userInput]); // Depend on userInput, not on currentBotMessage
-
   const initializeSession = async () => {
     setLoading(true);
     try {
@@ -79,34 +69,47 @@ const ChatWithLLM: React.FC = () => {
   };
 
   return (
-    <div className="chat-container">
-      {loading && <p>Loading...</p>}
-      <div className="messages">
-        {messages.map((message, index) => (
-          <div key={index} className={`message ${message.sender}`}>
-            {message.text}
-          </div>
-        ))}
-        {currentBotMessage && (
-          <div className="message bot">{currentBotMessage}</div>
-        )}
+    <div className="flex flex-col h-screen max-w-md mx-auto border shadow-lg overflow-hidden">
+      <div className={`flex-1 overflow-auto p-4 bg-white`}>
+        {loading && <p className="text-center text-gray-500">Loading...</p>}
+        <div className="space-y-2">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`p-2 rounded-lg ${
+                message.sender === "bot"
+                  ? "bg-blue-100 text-blue-800"
+                  : "bg-green-100 text-green-800"
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+          {currentBotMessage && (
+            <div className="p-2 rounded-lg bg-blue-100 text-blue-800">
+              {currentBotMessage}
+            </div>
+          )}
+        </div>
       </div>
-      <div className="input-area">
-        <input
-          type="text"
-          value={userInput}
-          onChange={handleUserInput}
-          className="input"
-          placeholder="Type your message..."
-          disabled={!sessionId || loading}
-        />
-        <button
-          className="submit-button"
-          onClick={handleSubmit}
-          disabled={!sessionId || loading}
-        >
-          Send
-        </button>
+      <div className="p-4 bg-gray-100">
+        <div className="flex space-x-2">
+          <input
+            type="text"
+            value={userInput}
+            onChange={handleUserInput}
+            className="flex-1 p-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Type your message..."
+            disabled={!sessionId || loading}
+          />
+          <button
+            className="px-4 py-2 text-white bg-blue-500 rounded shadow hover:bg-blue-600 disabled:bg-blue-300"
+            onClick={handleSubmit}
+            disabled={!sessionId || loading}
+          >
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
