ecba6a3edd73390723cf6076014d70e6530d6c2d
samlhuillier <sam.jansa123@gmail.com>
2023-11-27 14:07:10 -0600

    add in chat icon

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -4,6 +4,7 @@ import { HiSearch } from "react-icons/hi"; // Outlined search icon
 import { HiOutlineSearch } from "react-icons/hi"; // Solid search icon
 import SearchComponent from "./Search/Search";
 import NewNoteComponent from "./File/NewNote";
+import { MdChatBubble } from "react-icons/md";
 
 interface TitleBarProps {
   onFileSelect: (path: string) => void;
@@ -18,18 +19,20 @@ const TitleBar: React.FC<TitleBarProps> = ({ onFileSelect, toggleChatbot }) => {
     >
       <div className="ml-[90px] flex space-x-2">
         {/* <button className="bg-transparent border-none cursor-pointer">
-          <MdSearch className="text-gray-600" size={24} />
-        </button> */}
+        <MdSearch className="text-gray-600" size={24} />
+      </button> */}
         <SearchComponent onFileSelect={onFileSelect} />
         <button className="bg-transparent border-none cursor-pointer">
           <HiOutlinePlusCircle className="text-gray-600" size={24} />
         </button>
         <NewNoteComponent onFileSelect={onFileSelect} />
+      </div>
+      <div className="ml-auto">
         <button
           className="bg-transparent border-none cursor-pointer"
           onClick={toggleChatbot}
         >
-          Open chat
+          <MdChatBubble className="text-gray-600" size={24} />
         </button>
       </div>
     </div>
