77626d0d4d1d01e4c45182c7b24948cab33770cc
samlhuillier <sam.jansa123@gmail.com>
2023-11-30 15:46:17 -0600

    update name of initial settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import DirectoryPicker from "./components/File/DirectoryPicker";
+import DirectoryPicker from "./components/Settings/InitialSettingsPage";
 import FileEditorContainer from "./components/FileEditorContainer";
 
 interface AppProps {}
