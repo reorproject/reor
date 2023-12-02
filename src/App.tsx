3affbb11b589eec8f186f982a8b7c8138f38a09c
samlhuillier <sam.jansa123@gmail.com>
2023-12-02 17:50:30 -0600

    force an indexing every load of app

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC<AppProps> = () => {
     const initialDirectory = window.electronStore.getUserDirectory();
     if (initialDirectory) {
       setDirectory(initialDirectory);
+      window.database.indexFilesInDirectory(initialDirectory);
       // If directory exists, you may want to check if indexing is needed
     }
   }, []);
