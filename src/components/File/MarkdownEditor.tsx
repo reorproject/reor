cf969072b1416a24bc507f9e50ff9d3301d0f00c
samlhuillier <sam.jansa123@gmail.com>
2023-12-15 08:37:45 +0530

    remove old mdxeditor in favour of milkdown and remove all files relating to it

--- src/components/File/MarkdownEditor.tsx
+++ src/components/File/MarkdownEditor.tsx
@@ -1,271 +1,126 @@
-// // FileEditor.tsx
-// import "@mdxeditor/editor/style.css";
-// import React, { useEffect, useRef, useState } from "react";
-// import "./dark-editor.css";
+import React, { useEffect, useState } from "react";
+import {
+  Editor,
+  rootCtx,
+  defaultValueCtx,
+  editorViewCtx,
+  Ctx,
+  schemaCtx,
+} from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { commonmark } from "@milkdown/preset-commonmark";
+import { history } from "@milkdown/plugin-history";
+import { gfm } from "@milkdown/preset-gfm";
+import { ReactEditor, useEditor } from "@milkdown/react";
+import {
+  tooltip,
+  tooltipPlugin,
+  createToggleIcon,
+  defaultButtons,
+} from "@milkdown/plugin-tooltip";
+import "./milkdown.css";
+import { listener, listenerCtx } from "@milkdown/plugin-listener";
+import { prism } from "@milkdown/plugin-prism";
+import { menu } from "@milkdown/plugin-menu";
+import { block } from "@milkdown/plugin-block";
+// import slash from "./slash"; // Uncomment if slash is used
+import { cursor } from "@milkdown/plugin-cursor";
+import { clipboard } from "@milkdown/plugin-clipboard";
+import { insert, replaceAll, getMarkdown } from "@milkdown/utils";
 
-// import {
-//   AdmonitionDirectiveDescriptor,
-//   DiffSourceToggleWrapper,
-//   DirectiveDescriptor,
-//   KitchenSinkToolbar,
-//   MDXEditor,
-//   MDXEditorMethods,
-//   SandpackConfig,
-//   UndoRedo,
-//   codeBlockPlugin,
-//   codeMirrorPlugin,
-//   diffSourcePlugin,
-//   directivesPlugin,
-//   frontmatterPlugin,
-//   headingsPlugin,
-//   imagePlugin,
-//   linkDialogPlugin,
-//   linkPlugin,
-//   listsPlugin,
-//   markdownShortcutPlugin,
-//   quotePlugin,
-//   sandpackPlugin,
-//   tablePlugin,
-//   thematicBreakPlugin,
-//   toolbarPlugin,
-// } from "@mdxeditor/editor";
-// import { LeafDirective } from "mdast-util-directive";
+export interface MarkdownEditorProps {
+  filePath: string;
+  // content: string;
+  setContentInParent: (content: string) => void;
+  lastSavedContentRef: React.MutableRefObject<string>;
+}
 
-// export interface MarkdownEditorProps {
-//   filePath: string;
-//   // content: string;
-//   setContentInParent: (content: string) => void;
-//   lastSavedContentRef: React.MutableRefObject<string>;
-// }
+const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
+  filePath,
+  setContentInParent,
+  lastSavedContentRef,
+  // content,
+  // setContent,
+}) => {
+  const [content, setContent] = useState<string>("");
 
-// export const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
-//   filePath,
-//   // content,
-//   setContentInParent,
-//   lastSavedContentRef,
-// }) => {
-//   const [content, setContent] = useState<string>("");
-//   const ref = useRef<MDXEditorMethods>(null);
-//   // const lastSavedContentRef = useRef<string>("");
+  // const ref = useRef<MDXEditorMethods>(null);
+  // const lastSavedContentRef = useRef<string>("");
 
-//   useEffect(() => {
-//     const fetchContent = async () => {
-//       try {
-//         const fileContent = await window.files.readFile(filePath);
-//         setContent(fileContent);
-//         ref.current?.setMarkdown(fileContent);
-//         lastSavedContentRef.current = fileContent; // Initialize with fetched content
-//       } catch (error) {
-//         // Handle the error here
-//         console.error("Error reading file:", error);
-//         // Optionally, you can set some state to show an error message in the UI
-//       }
-//     };
+  const saveFile = async () => {
+    if (content !== lastSavedContentRef.current) {
+      // Check for changes since last save
+      console.log("calling save file:");
+      await window.files.writeFile(filePath, content);
+      lastSavedContentRef.current = content; // Update the ref to the latest saved content
+    }
+  };
 
-//     if (filePath) {
-//       fetchContent();
-//     }
-//   }, [filePath]);
+  useEffect(() => {
+    const saveInterval = setInterval(() => {
+      saveFile();
+    }, 5000); // Every 10 seconds
 
-//   const saveFile = async () => {
-//     if (content !== lastSavedContentRef.current) {
-//       // Check for changes since last save
-//       console.log("calling save file:");
-//       await window.files.writeFile(filePath, content);
-//       lastSavedContentRef.current = content; // Update the ref to the latest saved content
-//     }
-//   };
+    return () => clearInterval(saveInterval); // Clear the interval when component unmounts
+  }, [content]); // Dependency on content ensures saveFile has the latest content
 
-//   useEffect(() => {
-//     const saveInterval = setInterval(() => {
-//       saveFile();
-//     }, 5000); // Every 10 seconds
+  useEffect(() => {
+    // console.log("content set to: ")
+    setContentInParent(content);
+  }, [content]);
 
-//     return () => clearInterval(saveInterval); // Clear the interval when component unmounts
-//   }, [content]); // Dependency on content ensures saveFile has the latest content
+  const { editor, getInstance } = useEditor(
+    (root) =>
+      Editor.make()
+        .config((ctx) => {
+          ctx.set(rootCtx, root);
+          ctx.set(defaultValueCtx, content);
+          ctx
+            .get(listenerCtx)
 
-//   useEffect(() => {
-//     setContentInParent(content);
-//   }, [content]);
+            .markdownUpdated((ctx, markdown, prevMarkdown) => {
+              setContent(markdown);
+            });
+        })
 
-//   return (
-//     <div className="h-full overflow-y-auto">
-//       {/* <button onClick={() => ref.current?.setMarkdown("new markdown")}>
-//         Set new markdown
-//       </button>
-//       <button onClick={() => console.log(ref.current?.getMarkdown())}>
-//         Get markdown
-//       </button> */}
-//       {/* <MDXEditor ref={ref} markdown="hello world" onChange={console.log} />{" "} */}
-//       <MDXEditor
-//         ref={ref}
-//         className="dark-theme dark-editor"
-//         onChange={setContent}
-//         markdown={content}
-//         suppressHtmlProcessing={true}
-//         onError={(error) => console.log("error", error)}
-//         plugins={ALL_PLUGINS}
-//       />
-//       {/* <MDXEditor
-//         onChange={console.log}
-//         markdown={markdown}
-//         contentEditableClassName="prose"
-//         plugins={[
-//           // the viewMode parameter lets you switch the editor to diff or source mode.
-//           // you can get the diffMarkdown from your backend and pass it here.
-//           diffSourcePlugin({
-//             diffMarkdown: "An older version",
-//             viewMode: "rich-text",
-//           }),
-//           headingsPlugin(),
-//           listsPlugin(),
-//           toolbarPlugin({
-//             toolbarContents: () => (
-//               <DiffSourceToggleWrapper>
-//                 <UndoRedo />
-//               </DiffSourceToggleWrapper>
-//             ),
-//           }),
-//         ]}
-//       /> */}
-//       {/* <button onClick={saveFile} className="mt-4 bg-blue-500 text-white p-2">
-//         Save
-//       </button> */}
-//     </div>
-//   );
-// };
+        // .use(nord)
+        .use(commonmark)
+        // .use(gfm)
+        .use(history)
+        .use(listener)
+        .use(prism)
+        // .use(menu)
+        // .use(block)
+        .use(cursor)
+        .use(clipboard)
+    // .use(slash)
+  );
 
-// interface YoutubeDirectiveNode extends LeafDirective {
-//   name: "youtube";
-//   attributes: { id: string };
-// }
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const fileContent = await window.files.readFile(filePath);
+        // setContent(fileContent);
+        getInstance()?.action(replaceAll(fileContent));
+        // ref.current?.setMarkdown(fileContent);
+        lastSavedContentRef.current = fileContent; // Initialize with fetched content
+      } catch (error) {
+        // Handle the error here
+        console.error("Error reading file:", error);
+        // Optionally, you can set some state to show an error message in the UI
+      }
+    };
 
-// export const YoutubeDirectiveDescriptor: DirectiveDescriptor<YoutubeDirectiveNode> =
-//   {
-//     name: "youtube",
-//     type: "leafDirective",
-//     testNode(node) {
-//       return node.name === "youtube";
-//     },
-//     attributes: ["id"],
-//     hasChildren: false,
-//     Editor: ({ mdastNode, lexicalNode, parentEditor }) => {
-//       return (
-//         <div
-//           style={{
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "flex-start",
-//           }}
-//         >
-//           <button
-//             onClick={() => {
-//               parentEditor.update(() => {
-//                 lexicalNode.selectNext();
-//                 lexicalNode.remove();
-//               });
-//             }}
-//           >
-//             delete
-//           </button>
-//           <iframe
-//             width="560"
-//             height="315"
-//             src={`https://www.youtube.com/embed/${mdastNode.attributes?.id}`}
-//             title="YouTube video player"
-//             frameBorder="0"
-//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//           ></iframe>
-//         </div>
-//       );
-//     },
-//   };
+    if (filePath) {
+      fetchContent();
+    }
+  }, [filePath]);
 
-// const defaultSnippetContent = `
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>
-//   );
-// }
-// `.trim();
+  return (
+    <div className="h-fulloverflow-auto">
+      <ReactEditor editor={editor} />
+    </div>
+  );
+};
 
-// export const virtuosoSampleSandpackConfig: SandpackConfig = {
-//   defaultPreset: "react",
-//   presets: [
-//     {
-//       label: "React",
-//       name: "react",
-//       meta: "live react",
-//       sandpackTemplate: "react",
-//       sandpackTheme: "light",
-//       snippetFileName: "/App.js",
-//       snippetLanguage: "jsx",
-//       initialSnippetContent: defaultSnippetContent,
-//     },
-//     {
-//       label: "React",
-//       name: "react",
-//       meta: "live",
-//       sandpackTemplate: "react",
-//       sandpackTheme: "light",
-//       snippetFileName: "/App.js",
-//       snippetLanguage: "jsx",
-//       initialSnippetContent: defaultSnippetContent,
-//     },
-//     {
-//       label: "Virtuoso",
-//       name: "virtuoso",
-//       meta: "live virtuoso",
-//       sandpackTemplate: "react-ts",
-//       sandpackTheme: "light",
-//       snippetFileName: "/App.tsx",
-//       initialSnippetContent: defaultSnippetContent,
-//       dependencies: {
-//         "react-virtuoso": "latest",
-//         "@ngneat/falso": "latest",
-//       },
-//       // files: {
-//       //   "/data.ts": dataCode,
-//       // },
-//     },
-//   ],
-// };
-
-// export const ALL_PLUGINS = [
-//   // toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
-//   listsPlugin(),
-//   quotePlugin(),
-//   headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
-//   linkPlugin(),
-//   // linkDialogPlugin(),
-//   imagePlugin({
-//     imageAutocompleteSuggestions: [
-//       "https://via.placeholder.com/150",
-//       "https://via.placeholder.com/150",
-//     ],
-//   }),
-//   tablePlugin(),
-//   thematicBreakPlugin(),
-//   frontmatterPlugin(),
-//   codeBlockPlugin({ defaultCodeBlockLanguage: "txt" }),
-//   sandpackPlugin({ sandpackConfig: virtuosoSampleSandpackConfig }),
-//   codeMirrorPlugin({
-//     codeBlockLanguages: {
-//       js: "JavaScript",
-//       css: "CSS",
-//       txt: "text",
-//       tsx: "TypeScript",
-//     },
-//   }),
-//   directivesPlugin({
-//     directiveDescriptors: [
-//       YoutubeDirectiveDescriptor,
-//       AdmonitionDirectiveDescriptor,
-//     ],
-//   }),
-//   diffSourcePlugin({ viewMode: "rich-text", diffMarkdown: "boo" }),
-//   markdownShortcutPlugin(),
-// ];
+export default MarkdownEditor;
