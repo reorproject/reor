c7ece950fabf5734acf43a283d328e9d3f23c88c
Sam L’Huillier <sam@Sams-MacBook-Pro.local>
2024-01-27 22:47:36 +0000

    fix bug with cross & initial remote llm modal

--- src/components/Settings/LLMSettings.tsx
+++ src/components/Settings/LLMSettings.tsx
@@ -3,6 +3,7 @@ import { AIModelConfig } from "electron/main/Store/storeConfig";
 import CustomSelect from "../Generic/Select";
 import { Button } from "@material-tailwind/react";
 import LocalModelModal from "./NewLocalModel";
+import RemoteLLMModal from "./RemoteLLMSetup";
 
 interface LLMSettingsProps {
   userHasCompleted?: (completed: boolean) => void;
@@ -19,6 +20,9 @@ const LLMSettings: React.FC<LLMSettingsProps> = ({
     Record<string, AIModelConfig>
   >({});
   const [isNewLocalModelModalOpen, setIsNewLocalModelModalOpen] =
+    useState<boolean>(false);
+
+  const [isRemoteLLMModalOpen, setIsRemoteLLMModalOpen] =
     useState<boolean>(false);
 
   const [defaultModel, setDefaultModel] = useState<string>("");
@@ -83,14 +87,14 @@ const LLMSettings: React.FC<LLMSettingsProps> = ({
       <h4 className="text-gray-100 mb-1">Local LLM Settings:</h4>
       <div className="flex">
         <Button
-          className="bg-slate-700 border-none h-8 hover:bg-slate-900 cursor-pointer w-[180px] text-center pt-0 pb-0 pr-2 pl-2 mt-1 mr-4"
+          className="bg-slate-700 border-none h-8 hover:bg-slate-900 cursor-pointer w-full text-center pt-0 pb-0 pr-2 pl-2 mt-1 mr-4"
           onClick={() => setIsNewLocalModelModalOpen(true)}
           placeholder={""}
         >
           Add New Local LLM
         </Button>
         <Button
-          className="bg-slate-700 border-none h-8 hover:bg-slate-900 cursor-pointer w-[180px] text-center pt-0 pb-0 pr-2 pl-2 mt-1"
+          className="bg-slate-700 border-none h-8 hover:bg-slate-900 cursor-pointer w-full text-center pt-0 pb-0 pr-2 pl-2 mt-1"
           onClick={() => setIsNewLocalModelModalOpen(true)}
           placeholder={""}
         >
@@ -99,20 +103,26 @@ const LLMSettings: React.FC<LLMSettingsProps> = ({
       </div>
 
       <h4 className="text-gray-100 mb-0">Setup a new remote (OpenAI) LLM:</h4>
-      <Button
-        className="bg-slate-700  border-none h-8 hover:bg-slate-900 cursor-pointer w-[180px] text-center pt-0 pb-0 pr-2 pl-2 mt-2 mb-3"
-        onClick={() => setIsNewLocalModelModalOpen(true)}
-        placeholder=""
-      >
-        Remote LLM Setup
-      </Button>
-
+      <div className="flex">
+        <Button
+          className="bg-slate-700  border-none h-8 hover:bg-slate-900 cursor-pointer w-full text-center pt-0 pb-0 pr-2 pl-2 mt-2 mb-3 mr-4"
+          onClick={() => setIsNewLocalModelModalOpen(true)}
+          placeholder=""
+        >
+          Remote LLM Setup
+        </Button>
+        <div className="border-none h-8 w-full text-center pt-0 pb-0 pr-2 pl-2 mt-1"></div>
+      </div>
       <LocalModelModal
         isOpen={isNewLocalModelModalOpen}
         onClose={() => {
           setIsNewLocalModelModalOpen(false);
           fetchModelConfigs();
         }}
+      />
+      <RemoteLLMModal
+        isOpen={isRemoteLLMModalOpen}
+        onClose={() => setIsRemoteLLMModalOpen(false)}
       />
       {userTriedToSubmit && !defaultModel && (
         <p className="text-red-500 text-sm mt-1">{currentError}</p>
---
+++ src/components/Settings/RemoteLLMSetup.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Button } from "@material-tailwind/react";
+import Modal from "../Generic/Modal";
+import ExternalLink from "../Generic/ExternalLink";
+import { AIModelConfig } from "electron/main/Store/storeConfig";
+
+interface RemoteLLMModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const RemoteLLMModal: React.FC<RemoteLLMModalProps> = ({ isOpen, onClose }) => {
+  const [newModelContextLength, setNewModelContextLength] = useState<
+    number | null
+  >(null);
+  const [newModelPath, setNewModelPath] = useState<string>("");
+  const handleModelFileSelection = async () => {
+    const paths = await window.files.openFileDialog(["gguf"]);
+    if (paths && paths.length > 0) {
+      setNewModelPath(paths[0]);
+    }
+  };
+  const saveModelConfigToElectronStore = async () => {
+    if (!newModelPath || !newModelContextLength) {
+      return;
+    }
+    const newConfig: AIModelConfig = {
+      localPath: newModelPath,
+      contextLength: newModelContextLength,
+      engine: "llamacpp",
+    };
+
+    const res = await window.electronStore.setupNewLocalLLM(newConfig);
+    console.log("setupNewLocalLLM response: ", res);
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="w-[300px] ml-2 mr-2 mb-2">
+        <p className="text-white text-lg font-semibold mb-0">
+          Add New Local Model
+        </p>
+        <p className="text-white text-sm mb-2 mt-0">
+          Choose a .gguf model file on your computer to use as a local model.
+          You can download the best models from{" "}
+          <ExternalLink
+            url="https://huggingface.co/TheBloke?sort_models=downloads#models"
+            label="TheBloke on Huggingface"
+          />
+        </p>
+
+        <Button
+          className="bg-slate-700 border-none h-8 hover:bg-slate-900 cursor-pointer w-[180px] text-center pt-0 pb-0 pr-2 pl-2 mt-3"
+          onClick={handleModelFileSelection}
+          placeholder=""
+        >
+          Select Model .GGUF File
+        </Button>
+
+        {newModelPath && (
+          <p className="mt-2 text-xs text-gray-100">
+            Selected: <strong>{newModelPath}</strong>
+          </p>
+        )}
+
+        <input
+          className="w-full p-2 mb-1 mt-3 text-black box-border"
+          type="number"
+          placeholder="Context Length (in tokens)"
+          name="contextLength"
+          value={newModelContextLength || ""}
+          onChange={(e) => setNewModelContextLength(parseInt(e.target.value))}
+        />
+
+        <Button
+          className="bg-slate-700 border-none h-8 hover:bg-slate-900 cursor-pointer w-full text-center pt-0 pb-0 pr-2 pl-2 mt-3"
+          onClick={saveModelConfigToElectronStore}
+          placeholder=""
+        >
+          Save New Model
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default RemoteLLMModal;
