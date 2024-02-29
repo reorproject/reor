73d75c8aead2517e1d833267439e578432be1c05
Chu You Chia <chuyouchia@gmail.com>
2024-02-28 23:56:55 -0800

    update milkdown editor to use v7 and get indentations for free

--- src/components/File/MilkdownEditor.tsx
+++ src/components/File/MilkdownEditor.tsx
@@ -4,7 +4,7 @@ import { nord } from "@milkdown/theme-nord";
 import { commonmark } from "@milkdown/preset-commonmark";
 import { history } from "@milkdown/plugin-history";
 import { gfm } from "@milkdown/preset-gfm";
-import { ReactEditor, useEditor } from "@milkdown/react";
+import { useEditor, Milkdown, MilkdownProvider } from "@milkdown/react";
 import { listener, listenerCtx } from "@milkdown/plugin-listener";
 import { prism } from "@milkdown/plugin-prism";
 import { block } from "@milkdown/plugin-block";
@@ -44,38 +44,34 @@ const MilkdownEditor: React.FC<MarkdownEditorProps> = ({
     setContentInParent(content);
   }, [content]);
 
-  const { editor, getInstance } = useEditor(
-    (root) =>
-      Editor.make()
-        .config((ctx) => {
-          ctx.set(rootCtx, root);
-          ctx.set(defaultValueCtx, content);
-          ctx
-            .get(listenerCtx)
-
-            .markdownUpdated((ctx, markdown) => {
-              setContent(markdown);
-            });
+  const { get } = useEditor((root) => {
+    return Editor
+      .make()
+      .config(ctx => {
+        ctx.set(rootCtx, root)
+        ctx.set(defaultValueCtx, content);
+        ctx.get(listenerCtx).markdownUpdated((ctx, markdown) => {
+          setContent(markdown)
         })
-
-        .use(nord)
-        .use(commonmark)
-        .use(gfm)
-        .use(history)
-        .use(listener)
-        .use(prism)
-        // .use(menu)
-        .use(block)
-        .use(cursor)
-        .use(clipboard)
-    // .use(slash)
-  );
-
+      })
+      .config(nord)
+      .use(commonmark)
+      .use(gfm)
+      .use(history)
+      .use(listener)
+      .use(prism)
+      // // .use(menu)
+      .use(block)
+      .use(cursor)
+      .use(clipboard)
+      // .use(slash)
+  }, [])
+  
   useEffect(() => {
     const fetchContent = async () => {
       try {
         const fileContent = await window.files.readFile(filePath);
-        getInstance()?.action(replaceAll(fileContent));
+        get()?.action(replaceAll(fileContent));
         lastSavedContentRef.current = fileContent;
       } catch (error) {
         console.error("Error reading file:", error);
@@ -89,9 +85,21 @@ const MilkdownEditor: React.FC<MarkdownEditorProps> = ({
 
   return (
     <div className="h-full overflow-auto">
-      <ReactEditor editor={editor} />
+        <Milkdown />
     </div>
   );
 };
 
-export default MilkdownEditor;
+const MilkdownEditorWrapper: React.FC<MarkdownEditorProps> = ({
+  filePath,
+  setContentInParent,
+  lastSavedContentRef,
+}) => {
+  return (
+    <MilkdownProvider>
+      <MilkdownEditor filePath={filePath} setContentInParent={setContentInParent} lastSavedContentRef={lastSavedContentRef} />
+    </MilkdownProvider>
+  );
+};
+
+export default MilkdownEditorWrapper;
