2dd5887da73ff28e1db680ef9b0a159f5a749959
Chu You Chia <chuyou.chia@gmail.com>
2024-03-03 16:10:15 -0800

    add cursor pointer

--- src/components/Chat/Chat-Prompts.tsx
+++ src/components/Chat/Chat-Prompts.tsx
@@ -12,6 +12,7 @@ export const ChatPrompt: FC<Props> = ({ promptText, onClick } : Props) => {
         className={`
             rounded bg-gray-200 p-5 shadow dark:bg-gray-700 text-white text-base
             hover:bg-gray-300 hover:text-black mt-2
+            cursor-pointer
             border-solid border border-white border-opacity-50`}
           onClick={onClick}
       >
