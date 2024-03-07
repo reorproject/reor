6483fe529a6c332bd9cfff57db355e3972be6ac9
samlhuillier <sam.jansa123@gmail.com>
2024-03-07 20:58:41 +0000

    working implementation of saving and persisting content

--- src/components/File/TipTapEditor.tsx
+++ src/components/File/TipTapEditor.tsx
@@ -1,5 +1,5 @@
 import "@mdxeditor/editor/style.css";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./tiptap.scss";
 import { EditorProps } from "./MdxEditor";
 import Document from "@tiptap/extension-document";
@@ -7,21 +7,29 @@ import Paragraph from "@tiptap/extension-paragraph";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import Text from "@tiptap/extension-text";
+import TurndownService from "turndown";
+
 // import { Markdown } from "tiptap-markdown";
 
 // import "@toast-ui/editor/dist/toastui-editor.css";
+const turndownService = new TurndownService();
+turndownService.addRule("paragraph", {
+  filter: "p",
+  replacement: (content) => `\n\n${content}\n\n`, // Adjust replacement as needed
+});
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 // const extensions = [StarterKit];
 
-const tiptapContent = "Hello World!";
+// const tiptapContent = "Hello World!";
 
 export const TipTapEditor: React.FC<EditorProps> = ({
   filePath,
   setContentInParent,
   lastSavedContentRef,
 }) => {
+  // const [content, setContent] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const editor = useEditor({
     extensions: [
@@ -34,28 +42,35 @@ export const TipTapEditor: React.FC<EditorProps> = ({
         nested: true,
       }),
     ],
+    onUpdate: ({ editor }) => {
+      const htmlContent = editor?.getHTML();
+      console.log("htmlContent is: ", htmlContent);
+      // saveFile();
+      if (htmlContent) {
+        const markdown = turndownService.turndown(htmlContent);
+        console.log("markdown is: ", markdown);
+        if (markdown !== lastSavedContentRef.current) {
+          // setContentInParent(markdown);
+          setContent(markdown);
+        }
+      }
+      // send the content to an API here
+    },
 
-    content: tiptapContent,
+    // content: tiptapContent,
   });
-  // const ref = useRef(null);
 
-  // useEffect(() => {
-  //   console.log("content is: ", content);
-  //   if (editor) {
-  //     editor.commands.setContent(content);
-  //   }
-  // }, [editor, content]);
-
-  // editor?
+  useEffect(() => {
+    // setContentInParent(content);
+    console.log("content is: ", content);
+  }, [content]);
 
   useEffect(() => {
     const fetchContent = async () => {
       try {
         const fileContent = await window.files.readFile(filePath);
         setContent(fileContent);
-        // ref.current?.setMarkdown(fileContent);
-        editor?.commands.setContent(content);
-
+        editor?.commands.setContent(fileContent);
         lastSavedContentRef.current = fileContent; // Initialize with fetched content
       } catch (error) {
         // Handle the error here
@@ -67,44 +82,34 @@ export const TipTapEditor: React.FC<EditorProps> = ({
     if (filePath) {
       fetchContent();
     }
-  }, [editor, filePath]);
+  }, [filePath]);
 
   const saveFile = async () => {
-    const htmlContent = editor?.getHTML();
-    console.log("htmlContent is: ", htmlContent);
-    // if (content !== lastSavedContentRef.current) {
-    //   await window.files.writeFile({
-    //     filePath: filePath,
-    //     content: content,
-    //   });
-    //   lastSavedContentRef.current = content; // Update the ref to the latest saved content
-    // }
+    if (content !== lastSavedContentRef.current) {
+      await window.files.writeFile({
+        filePath: filePath,
+        content: content,
+      });
+      lastSavedContentRef.current = content; // Update the ref to the latest saved content
+    }
   };
 
   useEffect(() => {
     const saveInterval = setInterval(() => {
-      const htmlContent = editor?.getHTML();
-      console.log("htmlContent is: ", htmlContent);
-      // saveFile();
+      saveFile();
     }, 1000);
 
     return () => clearInterval(saveInterval); // Clear the interval when component unmounts
-  }, [editor, content]); // Dependency on content ensures saveFile has the latest content
+  }, [content]);
 
   useEffect(() => {
     setContentInParent(content);
   }, [content]);
 
-  const handleDivClick = () => {
-    // if (ref.current) {
-    // ref.current.focus(); // Programmatically trigger a click on the MDXEditor
-    // }
-  };
-
   return (
     <div
       className="h-full overflow-y-auto w-full cursor-text bg-slate-800 "
-      onClick={handleDivClick}
+      // onClick={handleDivClick}
     >
       <EditorContent editor={editor} />
       {/* <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu> */}
