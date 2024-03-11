14a9458a4f8748c7151e2f6ab568bdd20dad4955
samlhuillier <sam.jansa123@gmail.com>
2024-03-11 22:44:10 +0000

    move network functions into separate file

--- electron/main/download/download.ts
+++ electron/main/download/download.ts
@@ -1,7 +1,7 @@
-import { net, ClientRequestConstructorOptions } from "electron";
 import { listFiles, downloadFile } from "@huggingface/hub";
 import fs from "fs";
 import * as path from "path";
+import { customFetchUsingElectronNet } from "../Generic/network";
 
 export const DownloadModelFilesFromHFRepo = async (
   repo: string,
@@ -72,68 +72,3 @@ async function downloadAndSaveFile(
   fs.writeFileSync(fullPath, buffer);
   console.log(`Saved file to ${fullPath}`);
 }
-
-export const customFetchUsingElectronNet = async (
-  input: RequestInfo | URL,
-  init?: RequestInit
-): Promise<Response> => {
-  console.log("input: ", input);
-  console.log("init: ", init);
-  const url = input instanceof URL ? input.href : input.toString();
-  const options = init || {};
-
-  return new Promise((resolve, reject) => {
-    const requestOptions: ClientRequestConstructorOptions = {
-      method: options.method || "GET",
-      url: url,
-    };
-
-    const request = net.request(requestOptions);
-
-    // Set headers
-    if (options.headers) {
-      Object.entries(options.headers).forEach(([key, value]) => {
-        request.setHeader(key, value as string);
-      });
-    }
-
-    // Handle request body
-    if (options.body) {
-      let bodyData;
-      if (options.body instanceof ArrayBuffer) {
-        bodyData = Buffer.from(options.body);
-      } else if (
-        typeof options.body === "string" ||
-        Buffer.isBuffer(options.body)
-      ) {
-        bodyData = options.body;
-      } else if (typeof options.body === "object") {
-        bodyData = JSON.stringify(options.body);
-        request.setHeader("Content-Type", "application/json");
-      } else {
-        reject(new Error("Unsupported body type"));
-        return;
-      }
-      request.write(bodyData);
-    }
-
-    request.on("response", (response) => {
-      const chunks: Buffer[] = [];
-      response.on("data", (chunk) => chunks.push(chunk as Buffer));
-      response.on("end", () => {
-        const buffer = Buffer.concat(chunks);
-        resolve(
-          new Response(buffer, {
-            status: response.statusCode,
-            statusText: response.statusMessage,
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            headers: new Headers(response.headers as any),
-          })
-        );
-      });
-    });
-
-    request.on("error", (error) => reject(error));
-    request.end();
-  });
-};
--- electron/main/llm/llmSessionHandlers.ts
+++ electron/main/llm/llmSessionHandlers.ts
@@ -7,47 +7,15 @@ import {
   ChatCompletionChunk,
   ChatCompletionMessageParam,
 } from "openai/resources/chat/completions";
+// import { OllamaService } from "./models/Ollama";
 
 export const LLMSessions: { [sessionId: string]: LLMSessionService } = {};
 
 export const openAISession = new OpenAIModelSessionService();
 
+// export const ollamaSession = new OllamaService();
+
 export const registerLLMSessionHandlers = (store: Store<StoreSchema>) => {
-  // ipcMain.handle(
-  //   "does-session-exist",
-  //   async (event: IpcMainInvokeEvent, sessionId: string): Promise<boolean> => {
-  //     return !!LLMSessions[sessionId];
-  //   }
-  // );
-
-  // ipcMain.handle(
-  //   "delete-session",
-  //   async (event: IpcMainInvokeEvent, sessionId: string): Promise<string> => {
-  //     if (LLMSessions[sessionId]) {
-  //       const sessionService = LLMSessions[sessionId];
-  //       await sessionService.abort();
-  //       delete LLMSessions[sessionId];
-  //     }
-  //     return "success";
-  //   }
-  // );
-  // ipcMain.handle(
-  //   "create-session",
-  //   async (event: IpcMainInvokeEvent, sessionId: string): Promise<string> => {
-  //     return await "";
-  //   }
-  // );
-
-  // ipcMain.handle(
-  //   "get-or-create-session",
-  //   async (event: IpcMainInvokeEvent, sessionId: string): Promise<string> => {
-  //     if (LLMSessions[sessionId]) {
-  //       return sessionId;
-  //     }
-  //     return await createSession(store, sessionId);
-  //   }
-  // );
-
   ipcMain.handle(
     "streaming-llm-response",
     async (
@@ -73,39 +41,3 @@ export const registerLLMSessionHandlers = (store: Store<StoreSchema>) => {
     }
   );
 };
-
-// async function createSession( store: Store<StoreSchema>,
-//   sessionId: string
-// ): Promise<string> {
-//   throw new Error("Not implemented");
-//   // const defaultModelName = store.get(StoreKeys.DefaultLLM);
-//   // if (!defaultModelName) {
-//   //   throw new Error(
-//   //     "No default LLM model configured. Please choose either a local or a remote LLM in Settings."
-//   //   );
-//   // }
-//   // const allConfigs = store.get(StoreKeys.LLMs);
-
-//   // if (!allConfigs) {
-//   //   throw new Error("No AI models configured");
-//   // }
-
-//   // const currentModelConfig = allConfigs[defaultModelName];
-
-//   // const hardwareConfig = store.get(StoreKeys.Hardware);
-
-//   // if (currentModelConfig.type === "openai") {
-//   //   const sessionService = new OpenAIModelSessionService();
-//   //   await sessionService.init(defaultModelName, currentModelConfig);
-//   //   LLMSessions[sessionId] = sessionService;
-//   // } else {
-//   //   const sessionService = new LlamaCPPSessionService();
-//   //   await sessionService.init(
-//   //     defaultModelName,
-//   //     currentModelConfig,
-//   //     hardwareConfig
-//   //   );
-//   //   LLMSessions[sessionId] = sessionService;
-//   // }
-//   // return sessionId;
-// }
