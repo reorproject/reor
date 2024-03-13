7369f5e5301ddbc222de257094ee358ce774ba76
samlhuillier <sam.jansa123@gmail.com>
2024-03-13 18:28:59 +0000

    search sidebar is draggable for entirety of component

--- src/components/Sidebars/FileSidebarSearch.tsx
+++ src/components/Sidebars/FileSidebarSearch.tsx
@@ -37,7 +37,7 @@ const SearchComponent: React.FC<SearchComponentProps> = ({
   }, [searchQuery]);
 
   return (
-    <div className="p-0.5  w-full">
+    <div className="p-0.5 h-below-titlebar overflow-y-auto overflow-x-hidden">
       <div className="relative pr-0 pl-1 bg-gray-800 rounded-md mr-1">
         <span className="absolute inset-y-0 left-0 flex items-center pl-3 mt-[2px]">
           <FaSearch className="text-gray-200 text-lg" size={14} />
