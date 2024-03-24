30b1682e16f802b56bf0507843b9aa530ecf4915
Uğur Biçer <uuur86@yandex.com>
2024-03-25 00:30:02 +0300

    Enhance File Naming Conventions and Usability in File Listings Fixes #156

--- src/functions/strings.ts
+++ src/functions/strings.ts
@@ -10,6 +10,12 @@ export function removeFileExtension(filename: string): string {
   return filename.substring(0, filename.lastIndexOf("."));
 }
 
+export function readableFileName(filename: string): string {
+  return `-${removeFileExtension(filename)}`
+    .replace(/-[a-z]/g, (str) => str.replace("-", " ").toUpperCase())
+    .trim();
+}
+
 export const getInvalidCharacterInFileName = async (filename: string): Promise<string | null> => {
   let invalidCharacters: RegExp;
   const platform = await window.electron.getPlatform();
@@ -30,3 +36,28 @@ export const getInvalidCharacterInFileName = async (filename: string): Promise<s
 
   return idx === -1 ? null : filename[idx];
 }
+
+const charMap: { [key: string]: string } = {
+  'ä': 'a', 'Ä': 'A', 'ö': 'o', 'Ö': 'O', 'ü': 'u', 'Ü': 'U',
+  'ß': 'ss', 'é': 'e', 'è': 'e', 'ê': 'e', 'É': 'E', 'ë': 'e', 
+  'ı': 'i', 'İ': 'I', 'ğ': 'g', 'Ğ': 'G', 'ş': 's', 'Ş': 'S',
+  'α': 'a', 'β': 'b', 'γ': 'g', 'Γ': 'G', 'δ': 'd', 'Δ': 'D',
+  'ε': 'e', 'ζ': 'z', 'η': 'h', 'θ': 'th', 'ι': 'i', 'κ': 'k',
+  'λ': 'l', 'μ': 'm', 'ν': 'n', 'ξ': 'x', 'ο': 'o', 'π': 'p',
+  'ρ': 'r', 'σ': 's', 'τ': 't', 'υ': 'u', 'φ': 'f', 'χ': 'ch',
+  'ç': 'c', 'Ç': 'C', 'ψ': 'ps', 'ω': 'o', ' ': '-',
+};
+
+export const sanitizeFilename = (fileName: string) => {
+    // Remove invalid characters from the file name
+    const sanitizedFileName = fileName.toLowerCase().split('').map(char => 
+      charMap[char] || (/[a-z0-9\-]/.test(char) ? char : '')
+    ).join('').trim();
+    // Check if the sanitized filename is empty
+    if (sanitizedFileName.length > 0) {
+      // Limit the length of the filename (e.g., to 255 characters)
+      const maxLength = 255;
+      return sanitizedFileName.slice(0, maxLength).replace(/\\/g, "/");
+    }
+    return '';
+};
