ec253c984f396ddb0d6fd6226fb0f30f21afc005
samlhuillier <sam.jansa123@gmail.com>
2024-03-26 14:41:08 -0600

    update ollama wording for hardware page

--- src/components/Settings/HardwareSettings.tsx
+++ src/components/Settings/HardwareSettings.tsx
@@ -9,14 +9,15 @@ const HardwareSettings: React.FC<HardwareSettingsProps> = () => {
       <h2 className="text-2xl font-semibold mb-0">Hardware</h2>
       <p className="mt-2 text-sm text-gray-100 mb-1">
         Reor is now powered by{" "}
-        <ExternalLink href="https://ollama.com">Ollama</ExternalLink>. To get
-        Reor to use your Nvidia or AMD GPU, you will need to install Ollama and
-        make sure that it is running at the same time as Reor is. Check out
-        Ollama&apos;s{" "}
+        <ExternalLink href="https://ollama.com">Ollama</ExternalLink>. That
+        means, you will need to install{" "}
+        <ExternalLink href="https://ollama.com">Ollama</ExternalLink> and make
+        sure that it is running at the same time as Reor is to use your Nvidia
+        or AMD GPU. Check out Ollama&apos;s{" "}
         <ExternalLink href="https://github.com/ollama/ollama/blob/main/docs/gpu.md">
           docs page
         </ExternalLink>{" "}
-        for more info on configuring your GPU.
+        for more info on GPUs.
       </p>
     </div>
   );
