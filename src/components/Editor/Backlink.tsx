7e397f780c672481f6b5f820f4d65cb3efaaf18a
samlhuillier <sam.jansa123@gmail.com>
2024-04-01 10:53:22 -0600

    switch to tiptap imports

--- src/components/Editor/Backlink.tsx
+++ src/components/Editor/Backlink.tsx
@@ -1,6 +1,6 @@
 import { Extension } from "@tiptap/core";
-import { Plugin, PluginKey } from "prosemirror-state";
-import { Decoration, DecorationSet } from "prosemirror-view";
+import { Plugin, PluginKey } from "@tiptap/pm/state";
+import { Decoration, DecorationSet } from "@tiptap/pm/view";
 
 const backlinkPlugin = new Plugin({
   key: new PluginKey("backlinks"),
