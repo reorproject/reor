89519fc0c6cb72e7560521061e9e5cb8122904a4
samlhuillier <sam.jansa123@gmail.com>
2024-04-17 12:32:43 -0600

    fix lint

--- src/components/FileEditorContainer.tsx
+++ src/components/FileEditorContainer.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import SimilarEntriesComponent from "./Similarity/SimilarFilesSidebar";
+import React, { useState } from "react";
 import TitleBar from "./TitleBar";
 import ChatWithLLM from "./Chat/Chat";
 import LeftSidebar from "./Sidebars/IconsSidebar";
