2e77998f6f2a44dec8f2348614fefeeb17ee186c
samlhuillier <sam.jansa123@gmail.com>
2024-04-23 16:16:33 -0700

    move chat input to its own component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -18,6 +18,7 @@ import {
   ChatCompletionMessageParam,
 } from "openai/resources/chat/completions";
 import { DBQueryResult } from "electron/main/database/Schema";
+import ChatInput from "./ChatInput";
 
 // convert ask options to enum
 enum AskOptions {
@@ -361,84 +362,15 @@ const ChatWithLLM: React.FC<ChatWithLLMProps> = ({
           ) : undefined}
         </div>
 
-        <div className="p-3 bg-neutral-600">
-          <div className="flex space-x-2 h-full">
-            <Textarea
-              onKeyDown={(e) => {
-                if (!e.shiftKey && e.key === "Enter") {
-                  e.preventDefault();
-                  handleSubmitNewMessage(currentChatHistory);
-                }
-              }}
-              onChange={(e) => setUserTextFieldInput(e.target.value)}
-              value={userTextFieldInput}
-              className="w-full  bg-gray-300"
-              name="Outlined"
-              placeholder="Ask your knowledge..."
-              variant="outlined"
-              style={{
-                backgroundColor: "rgb(64 64 64)",
-                color: "rgb(212 212 212)",
-              }}
-            />
-            <div className="flex justify-center items-center h-full ">
-              {loadingResponse ? (
-                <CircularProgress
-                  size={32}
-                  thickness={20}
-                  style={{ color: "rgb(209 213 219 / var(--tw-bg-opacity))" }}
-                  className="h-full w-full m-x-auto color-gray-500 "
-                />
-              ) : (
-                <>
-                  <button
-                    aria-expanded="false"
-                    aria-haspopup="menu"
-                    className={`align-middle select-none font-sans font-bold transition-all 
-                  text-xs py-3 px-6 rounded-tl rounded-bl text-white shadow-md shadow-gray-900/10 
-                  hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] 
-                  active:shadow-none bg-neutral-700 border-none h-full hover:bg-neutral-900 cursor-pointer text-center 
-                  pt-0 pb-0 pr-2 pl-2`}
-                    type="button"
-                    onClick={() => handleSubmitNewMessage(currentChatHistory)}
-                  >
-                    {askText}
-                  </button>
-                  <Menu placement="top-end">
-                    <MenuHandler>
-                      <button
-                        aria-expanded="false"
-                        aria-haspopup="menu"
-                        className={`align-middle select-none font-sans font-bold uppercase transition-all 
-                  text-xs py-3 px-6 rounded-tr rounded-br text-white shadow-md shadow-gray-900/10 
-                  hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] 
-                  active:shadow-none bg-neutral-700 border-none h-full hover:bg-neutral-900 cursor-pointer text-center 
-                  pt-0 pb-0 pr-2 pl-2`}
-                        type="button"
-                      >
-                        <div className="mb-1">⌄</div>
-                      </button>
-                    </MenuHandler>
-                    <MenuList placeholder="" className="bg-neutral-600">
-                      {ASK_OPTIONS.map((option, index) => {
-                        return (
-                          <MenuItem
-                            key={index}
-                            placeholder=""
-                            className="bg-neutral-600 border-none h-full w-full hover:bg-neutral-700 cursor-pointer text-white text-left p-2"
-                            onClick={() => setAskText(option)}
-                          >
-                            {option}
-                          </MenuItem>
-                        );
-                      })}
-                    </MenuList>
-                  </Menu>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
+        <ChatInput
+          userTextFieldInput={userTextFieldInput}
+          setUserTextFieldInput={setUserTextFieldInput}
+          handleSubmitNewMessage={() =>
+            handleSubmitNewMessage(currentChatHistory)
+          }
+          loadingResponse={loadingResponse}
+          askText={askText}
+        />
       </div>
     </div>
   );
