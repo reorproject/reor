10f9f067667e71395052ae4804b0c1f5b6b7c825
MoTheRoar <milaiwi@outlook.com>
2024-06-11 13:30:11 -0500

    Added LaTex support. Need to manually configure to add support for Matrices

--- src/components/File/hooks/use-file-by-filepath.ts
+++ src/components/File/hooks/use-file-by-filepath.ts
@@ -9,6 +9,7 @@ import { Editor, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { toast } from "react-toastify";
 import { Markdown } from "tiptap-markdown";
+import { MathExtension } from "@aarkue/tiptap-math-extension";
 import { useDebounce } from "use-debounce";
 
 import { BacklinkExtension } from "@/components/Editor/BacklinkExtension";
@@ -22,6 +23,7 @@ import {
   removeFileExtension,
 } from "@/functions/strings";
 import "../tiptap.scss";
+import "katex/dist/katex.min.css";
 
 export const useFileByFilepath = () => {
   const [currentlyOpenedFilePath, setCurrentlyOpenedFilePath] = useState<
@@ -130,6 +132,9 @@ export const useFileByFilepath = () => {
       Paragraph,
       Text,
       TaskList,
+      MathExtension.configure({
+        evaluation: true,
+      }),
       Markdown.configure({
         html: true, // Allow HTML input/output
         tightLists: true, // No <p> inside <li> in markdown output
