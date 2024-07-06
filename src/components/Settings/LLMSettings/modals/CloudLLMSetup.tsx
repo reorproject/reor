051ccdb9ebe1dcf85234da2cb99571c2cee10670
joseplayero <claudeshannon11@outlook.com>
2024-07-06 23:19:29 +0100

    fix colors

--- src/components/Settings/LLMSettings/modals/CloudLLMSetup.tsx
+++ src/components/Settings/LLMSettings/modals/CloudLLMSetup.tsx
@@ -83,7 +83,7 @@ const CloudLLMSetupModal: React.FC<CloudLLMSetupModalProps> = ({
         </p>
 
         <Button
-          className="bg-orange-700 border-none h-8 hover:bg-orange-900 cursor-pointer text-center pt-0 pb-0 pr-2 pl-2 mt-1 w-[80px]"
+          className="bg-blue-300  border-none h-8 hover:bg-blue-400 cursor-pointer text-center pt-0 pb-0 pr-2 pl-2 mt-1 w-[80px]"
           onClick={handleSave}
           placeholder=""
         >
