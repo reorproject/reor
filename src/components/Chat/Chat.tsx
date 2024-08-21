0c98ea8e124395992913cfb6cdfff5dbbc34322a
MoTheRoar <burkmoh@gmail.com>
2024-08-20 21:16:29 -0500

    Cleaned up codebase

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -28,11 +28,7 @@ import '../../styles/chat.css'
 // convert ask options to enum
 enum AskOptions {
   Ask = 'Ask',
-  // AskFile = "Ask File",
-  // TemporalAsk = "Temporal Ask",
-  // FlashcardAsk = "Flashcard Ask",
 }
-// const ASK_OPTIONS = Object.values(AskOptions);
 
 const EXAMPLE_PROMPTS: { [key: string]: string[] } = {
   [AskOptions.Ask]: [
@@ -68,6 +64,22 @@ interface ChatWithLLMProps {
   showSimilarFiles: boolean
   chatFilters: ChatFilters
   setChatFilters: React.Dispatch<ChatFilters>
+}
+
+interface ChatContainerProps {
+  chatContainerRef: MutableRefObject<HTMLDivElement | null>
+  currentChatHistory: ChatHistory | undefined
+  handleSubmitNewMessage: () => void
+  loadAnimation: boolean
+  isAddContextFiltersModalOpen: boolean
+  chatFilters: ChatFIlters
+  setChatFilters: Dispatch<SetStateAction<ChatFilters>>
+  setUserTextFieldInput: Dispatch<SetStateAction<string>>
+  defaultModelName: string
+  vaultDirectory: string
+  setIsAddContextFiltersModalOpen: Dispatch<SetStateAction<boolean>>
+  handlePromptSelection: (prompt: string) => void
+  askText: AskOptions
 }
 
 const ChatWithLLM: React.FC<ChatWithLLMProps> = ({
@@ -183,8 +195,7 @@ const ChatWithLLM: React.FC<ChatWithLLMProps> = ({
     let outputChatHistory = chatHistory
 
     try {
-      if (loadingResponse) return
-      if (!userTextFieldInput.trim()) return
+      if (loadingResponse || !userTextFieldInput.trim()) return
       const defaultLLMName = await window.llm.getDefaultLLMName()
       if (!outputChatHistory || !outputChatHistory.id) {
         const chatID = Date.now().toString()
@@ -273,20 +284,14 @@ const ChatWithLLM: React.FC<ChatWithLLMProps> = ({
     }
   }, [triggerUpdate])
 
-  const getClassName = (message: ChatMessageToDisplay): string => {
-    return message.messageType === 'error'
-      ? `markdown-content ${message.messageType}-chat-message`
-      : `markdown-content ${message.role}-chat-message`
-  }
 
-  const handlePromptSelection = (prompt: string) => {
+  const handlePromptSelection = useCallback((prompt: string) => {
     setUserTextFieldInput(prompt)
     setTriggerUpdate(true)
-  }
+  }, [setUserTextFieldInput, setTriggerUpdate])
 
 
   const stopStreamingResponse = () => {
-    console.log("Stopped streaming!")
     setLoadAnimation(false)
     setLoadingResponse(false)
   }
@@ -303,118 +308,23 @@ const ChatWithLLM: React.FC<ChatWithLLMProps> = ({
   }, [currentChatHistory]);
 
   return (
-    <div 
-      className="flex size-full items-center justify-center"
-    >
+    <div className="flex size-full items-center justify-center">
       <div className="mx-auto flex size-full flex-col overflow-hidden border-y-0 border-l-[0.001px] border-r-0 border-solid border-neutral-700 bg-dark-gray-c-eleven">
-        <div 
-          ref={chatContainerRef}
-          className="chat-container relative flex h-full flex-col items-center justify-center overflow-auto bg-transparent">
-          <div
-            className={`relative mt-4 flex size-full flex-col gap-3 overflow-x-hidden p-10 pt-0 items-center`}
-          >
-            <div className={`w-full max-w-3xl flex-col items-center`}>
-              {currentChatHistory && currentChatHistory.displayableChatHistory.length > 0 ? (
-                // Display chat history if it exists
-                currentChatHistory.displayableChatHistory
-                  .filter((msg) => msg.role !== 'system')
-                  .map((message, index) => (
-                    <div className={`w-full ${getClassName(message)} flex`}>
-                      <div className="relative items-start pl-4 pt-3">
-                        {message.role === 'user' ? (
-                          <FaRegUserCircle size={22} />
-                        ) : (
-                          <img
-                            src="/src/assets/reor-logo.svg"
-                            style={{ width: '22px', height: '22px' }}
-                            alt="ReorImage"
-                          />
-                        )}
-                      </div>
-                      <div className="w-full flex-col gap-1">
-                        <div className="flex grow flex-col px-5 py-2.5">
-                          <ReactMarkdown
-                            // eslint-disable-next-line react/no-array-index-key
-                            key={index}
-                            rehypePlugins={[rehypeRaw]}
-                            className="max-w-[95%] break-words"
-                          >
-                            {message.visibleContent
-                              ? message.visibleContent
-                              : formatOpenAIMessageContentIntoString(message.content)}
-                          </ReactMarkdown>
-                        </div>
-                      </div>
-                    </div>
-                  ))
-              ) : (
-                // Display centered "Start a conversation..." if there is no currentChatHistory
-                <div className="relative flex flex-col w-full">
-                  <div className="relative flex size-full flex-col text-center lg:top-10">
-                    <div className="size-full flex justify-center">
-                      <img src="/src/assets/reor-logo.svg" style={{ width: '64px', height: '64px' }} alt="ReorImage" />
-                    </div>
-                    <h1 className="mb-10 text-gray-300 text-[28px]">Welcome to your AI-powered assistant! Start your first conversation or pick up where you left off.</h1>
-                    <div className="flex flex-col rounded-md bg-bg-000 focus-within:ring focus-within:ring-gray-700">
-                      <textarea
-                        onKeyDown={(e) => {
-                          if (!e.shiftKey && e.key === 'Enter') {
-                            e.preventDefault()
-                            handleSubmitNewMessage(undefined)
-                          }
-                        }}
-                        className="h-[100px] w-full resize-none rounded-t-md border-0 bg-transparent p-4 font-styrene text-text-gen-100 caret-white focus:outline-none"
-                        placeholder="What can Reor help you with today?"
-                        onChange={(e) => setUserTextFieldInput(e.target.value)}
-                      />
-                      <div className="h-px w-[calc(100%-5%)] self-center bg-gray-600" />
-                      <div className="flex items-center justify-between px-4 py-3 ">
-                        <span className="rounded-b-md bg-transparent  font-styrene text-sm tracking-tight text-text-gen-100">
-                          {defaultModelName}
-                        </span>
-                        <button
-                          className="cursor-pointer rounded-md border-0 bg-blue-600 px-4 py-2 font-styrene text-white hover:bg-blue-500"
-                          onClick={() => {
-                            setIsAddContextFiltersModalOpen(true)
-                          }}
-                          type="button"
-                        >
-                          {chatFilters.files.length > 0 ? 'Update RAG filters' : 'Customise context'}
-                        </button>
-                      </div>
-                    </div>
-                    <div className="size-full mt-4 lg:flex md:flex-row justify-center">
-                      {EXAMPLE_PROMPTS[askText].map((option) => (
-                          <PromptSuggestion
-                            key={option}
-                            promptText={option}
-                            onClick={() => handlePromptSelection(option)}
-                          />
-                        ))}
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-
-            {loadAnimation && (
-              <div className="relative max-w-3xl left-4 ml-1 mt-4 flex w-full items-start gap-6">
-                <img src="/src/assets/reor-logo.svg" style={{ width: '22px', height: '22px' }} alt="ReorImage" />
-                <LoadingDots />
-              </div>
-            )}
-
-            {isAddContextFiltersModalOpen && (
-              <AddContextFiltersModal
-                vaultDirectory={vaultDirectory}
-                isOpen={isAddContextFiltersModalOpen}
-                onClose={() => setIsAddContextFiltersModalOpen(false)}
-                chatFilters={chatFilters}
-                setChatFilters={setChatFilters}
-              />
-            )}
-          </div>
-        </div>
+        <ChatContainer
+          chatContainerRef={chatContainerRef}
+          currentChatHistory={currentChatHistory}
+          handleSubmitNewMessage={handleSubmitNewMessage}
+          loadAnimation={loadAnimation}
+          isAddContextFiltersModalOpen={isAddContextFiltersModalOpen}
+          chatFilters={chatFilters}
+          setChatFilters={setChatFilters}
+          setUserTextFieldInput={setUserTextFieldInput}
+          defaultModelName={defaultModelName}
+          vaultDirectory={vaultDirectory}
+          setIsAddContextFiltersModalOpen={setIsAddContextFiltersModalOpen}
+          handlePromptSelection={handlePromptSelection}
+          askText={askText}
+        />
 
         {currentChatHistory && (
           <ChatInput
@@ -437,6 +347,140 @@ const ChatWithLLM: React.FC<ChatWithLLMProps> = ({
           isLoadingSimilarEntries={false}
         />
       )}
+    </div>
+  )
+}
+
+
+const ChatContainer: React.FV<ChatContainerProps> = ({
+  chatContainerRef,
+  currentChatHistory,
+  handleSubmitNewMessage,
+  loadAnimation,
+  isAddContextFiltersModalOpen,
+  chatFilters,
+  setChatFilters,
+  setUserTextFieldInput,
+  defaultModelName,
+  vaultDirectory,
+  setIsAddContextFiltersModalOpen,
+  handlePromptSelection,
+  askText
+}) => {
+  const getClassName = (message: ChatMessageToDisplay): string => {
+    return message.messageType === 'error'
+      ? `markdown-content ${message.messageType}-chat-message text-white`
+      : `markdown-content ${message.role}-chat-message`
+  }
+
+  return (
+    <div 
+      ref={chatContainerRef}
+      className="chat-container relative flex h-full flex-col items-center justify-center overflow-auto bg-transparent">
+      <div
+        className={`relative mt-4 flex size-full flex-col gap-3 overflow-x-hidden p-10 pt-0 items-center`}
+      >
+        <div className={`w-full max-w-3xl flex-col items-center`}>
+          {currentChatHistory && currentChatHistory.displayableChatHistory.length > 0 ? (
+            // Display chat history if it exists
+            currentChatHistory.displayableChatHistory
+              .filter((msg) => msg.role !== 'system')
+              .map((message, index) => (
+                <div className={`w-full ${getClassName(message)} flex`}>
+                  <div className="relative items-start pl-4 pt-3">
+                    {message.role === 'user' ? (
+                      <FaRegUserCircle size={22} />
+                    ) : (
+                      <img
+                        src="/src/assets/reor-logo.svg"
+                        style={{ width: '22px', height: '22px' }}
+                        alt="ReorImage"
+                      />
+                    )}
+                  </div>
+                  <div className="w-full flex-col gap-1">
+                    <div className="flex grow flex-col px-5 py-2.5">
+                      <ReactMarkdown
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={index}
+                        rehypePlugins={[rehypeRaw]}
+                        className="max-w-[95%] break-words"
+                      >
+                        {message.visibleContent
+                          ? message.visibleContent
+                          : formatOpenAIMessageContentIntoString(message.content)}
+                      </ReactMarkdown>
+                    </div>
+                  </div>
+                </div>
+              ))
+          ) : (
+            // Display centered "Start a conversation..." if there is no currentChatHistory
+            <div className="relative flex flex-col w-full">
+              <div className="relative flex size-full flex-col text-center lg:top-10">
+                <div className="size-full flex justify-center">
+                  <img src="/src/assets/reor-logo.svg" style={{ width: '64px', height: '64px' }} alt="ReorImage" />
+                </div>
+                <h1 className="mb-10 text-gray-300 text-[28px]">Welcome to your AI-powered assistant! Start your first conversation or pick up where you left off.</h1>
+                <div className="flex flex-col rounded-md bg-bg-000 focus-within:ring focus-within:ring-gray-700">
+                  <textarea
+                    onKeyDown={(e) => {
+                      if (!e.shiftKey && e.key === 'Enter') {
+                        e.preventDefault()
+                        handleSubmitNewMessage(undefined)
+                      }
+                    }}
+                    className="h-[100px] w-full resize-none rounded-t-md border-0 bg-transparent p-4 font-styrene text-text-gen-100 caret-white focus:outline-none"
+                    placeholder="What can Reor help you with today?"
+                    onChange={(e) => setUserTextFieldInput(e.target.value)}
+                  />
+                  <div className="h-px w-[calc(100%-5%)] self-center bg-gray-600" />
+                  <div className="flex items-center justify-between px-4 py-3 ">
+                    <span className="rounded-b-md bg-transparent  font-styrene text-sm tracking-tight text-text-gen-100">
+                      {defaultModelName}
+                    </span>
+                    <button
+                      className="cursor-pointer rounded-md border-0 bg-blue-600 px-4 py-2 font-styrene text-white hover:bg-blue-500"
+                      onClick={() => {
+                        setIsAddContextFiltersModalOpen(true)
+                      }}
+                      type="button"
+                    >
+                      {chatFilters.files.length > 0 ? 'Update RAG filters' : 'Customise context'}
+                    </button>
+                  </div>
+                </div>
+                <div className="size-full mt-4 lg:flex md:flex-row justify-center">
+                  {EXAMPLE_PROMPTS[askText].map((option) => (
+                      <PromptSuggestion
+                        key={option}
+                        promptText={option}
+                        onClick={() => handlePromptSelection(option)}
+                      />
+                    ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {loadAnimation && (
+          <div className="relative max-w-3xl left-4 ml-1 mt-4 flex w-full items-start gap-6">
+            <img src="/src/assets/reor-logo.svg" style={{ width: '22px', height: '22px' }} alt="ReorImage" />
+            <LoadingDots />
+          </div>
+        )}
+
+        {isAddContextFiltersModalOpen && (
+          <AddContextFiltersModal
+            vaultDirectory={vaultDirectory}
+            isOpen={isAddContextFiltersModalOpen}
+            onClose={() => setIsAddContextFiltersModalOpen(false)}
+            chatFilters={chatFilters}
+            setChatFilters={setChatFilters}
+          />
+        )}
+      </div>
     </div>
   )
 }
