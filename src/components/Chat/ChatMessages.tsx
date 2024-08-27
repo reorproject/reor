57ae439dcb2ed0985fa5d298b8e253e0ef3594a2
Dheeraj Singh <singhdheeraj2696@gmail.com>
2024-08-27 16:23:15 +0530

    added changes to update the default llm model according to the chosen llm from dropdown  while sending message

--- src/components/Chat/ChatMessages.tsx
+++ src/components/Chat/ChatMessages.tsx
@@ -89,10 +89,10 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
     fetchLLMModels()
   }, [])
 
-  const sendMessageButtonHandler = (e: any) => {
+  const sendMessageButtonHandler = async(e: any) => {
     // Logic to send message button to the selected LLM
     e.preventDefault()
-
+    await window.ipcRenderer.invoke("set-default-llm",selectedLlm)
     handlePromptSelection(undefined)
   }
 
