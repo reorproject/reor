e3d25a627b622099b4ad9a5da0d973e43c446f81
samlhuillier <sam.jansa123@gmail.com>
2024-10-21 15:22:56 -0600

    fix markdown renderer spacing

--- src/components/Common/MarkdownRenderer.tsx
+++ src/components/Common/MarkdownRenderer.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/heading-has-content */
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import rehypeRaw from 'rehype-raw'
 
-interface MarkdownRendererProps {
-  content: string
+const customComponents = {
+  h1: ({ ...props }: React.ComponentPropsWithoutRef<'h1'>) => <h1 className="my-4 text-2xl font-bold" {...props} />,
+  h2: ({ ...props }: React.ComponentPropsWithoutRef<'h2'>) => <h2 className="my-3 text-xl font-semibold" {...props} />,
+  h3: ({ ...props }: React.ComponentPropsWithoutRef<'h3'>) => <h3 className="my-2 text-lg font-medium" {...props} />,
 }
 
-const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
+const MarkdownRenderer: React.FC<{ content: string }> = ({ content }) => {
   return (
     <ReactMarkdown
       className="overflow-hidden whitespace-normal break-words text-muted-foreground"
-      rehypePlugins={[rehypeRaw]}
+      components={customComponents}
     >
       {content}
     </ReactMarkdown>
