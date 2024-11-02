edd2875ee744f2fd163e04235d3126e4744df4fc
samlhuillier <sam.jansa123@gmail.com>
2024-11-02 22:16:08 +0000

    initial changes for window controls

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable no-nested-ternary */
 import React, { useState } from 'react'
+import { X, Maximize2 } from 'lucide-react'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 
 import '../styles/global.css'
@@ -10,7 +13,7 @@ import EditorManager from './Editor/EditorManager'
 import IconsSidebar from './Sidebars/IconsSidebar'
 import SidebarManager from './Sidebars/MainSidebar'
 import EmptyPage from './Common/EmptyPage'
-import { ContentProvider } from '../contexts/ContentContext'
+import { ContentProvider, useContentContext } from '../contexts/ContentContext'
 import WritingAssistant from './WritingAssistant/WritingAssistant'
 import { ChatProvider, useChatContext } from '@/contexts/ChatContext'
 import { FileProvider, useFileContext } from '@/contexts/FileContext'
@@ -18,18 +21,41 @@ import ModalProvider from '@/contexts/ModalContext'
 import CommonModals from './Common/CommonModals'
 import useAppShortcuts from './shortcuts/use-shortcut'
 
+const WindowControls: React.FC<{
+  onClose: () => void
+  onMaximize: () => void
+}> = ({ onClose, onMaximize }) => (
+  <div className="absolute right-2 top-2 z-10 flex gap-2">
+    <button onClick={onMaximize} className="rounded p-1 transition-colors hover:bg-neutral-700/50" title="Maximize">
+      <Maximize2 className="size-4 text-neutral-400" />
+    </button>
+    <button onClick={onClose} className="rounded p-1 transition-colors hover:bg-neutral-700/50" title="Close">
+      <X className="size-4 text-neutral-400" />
+    </button>
+  </div>
+)
+
 // Moved MainContent outside as a separate component
 const MainContent: React.FC = () => {
   const { currentlyOpenFilePath } = useFileContext()
+  const { setShowChatbot } = useChatContext()
+  const { showEditor, setShowEditor } = useContentContext()
 
   return (
     <div className="relative flex size-full overflow-hidden">
-      {currentlyOpenFilePath ? (
+      {currentlyOpenFilePath && showEditor ? (
         <div className="size-full overflow-hidden">
+          <WindowControls
+            onClose={() => setShowEditor(false)}
+            onMaximize={() => {
+              setShowChatbot(false)
+            }}
+          />
           <EditorManager />
         </div>
       ) : (
-        <EmptyPage />
+        // <EmptyPage />
+        <></>
       )}
       <WritingAssistant />
     </div>
@@ -39,7 +65,8 @@ const MainContent: React.FC = () => {
 const MainPageContent: React.FC = () => {
   const [showSimilarFiles, setShowSimilarFiles] = useState(false)
   const { currentlyOpenFilePath } = useFileContext()
-  const { showChatbot } = useChatContext()
+  const { showChatbot, setShowChatbot } = useChatContext()
+  const { setShowEditor } = useContentContext()
   const { getShortcutDescription } = useAppShortcuts()
 
   return (
@@ -70,7 +97,15 @@ const MainPageContent: React.FC = () => {
                       </ResizablePanel>
                       <ResizableHandle />
                       <ResizablePanel defaultSize={35}>
-                        <div className="size-full bg-pink-200">
+                        <div className="relative size-full bg-pink-200">
+                          <WindowControls
+                            onClose={() => {
+                              setShowChatbot(false)
+                            }}
+                            onMaximize={() => {
+                              setShowEditor(false)
+                            }}
+                          />
                           <ChatComponent />
                         </div>
                       </ResizablePanel>
